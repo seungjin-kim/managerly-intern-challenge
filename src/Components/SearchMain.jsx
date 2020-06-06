a463035ad24ae629d6ae63e7edfcd4d5b706a9ff
SeungJin Kim <seungjinkim@SeungJins-MBP.attlocal.net>
2020-06-06 15:45:59 -0700

    Create SearchMain class

---
+++ src/Components/SearchMain.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+export default class SearchMain extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+
+    };
+  }
+
+  componentDidMount() {
+
+  }
+}
