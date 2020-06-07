6f1e082219b090484c27db7c2e30a155b352f747
SeungJin Kim <seungjinkim@SeungJins-MBP.attlocal.net>
2020-06-06 18:21:49 -0700

    Add getPopularVideos method and create DOM skeleton to render

--- src/Components/SearchMain.jsx
+++ src/Components/SearchMain.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import axios from 'axios';
+import YOUTUBE_API_KEY from '../config/youtube.js'
+import VideoResults from './VideoResults.jsx'
 
 
 export default class SearchMain extends React.Component {
@@ -7,23 +9,44 @@ export default class SearchMain extends React.Component {
     super(props);
 
     this.state = {
-      
+      videos: [],
     };
   }
 
   componentDidMount() {
-    this.getPopularVideos();
+    console.log("Component did mount successful")
+    this.getPopularVideos()
   }
 
   getPopularVideos() {
-
+    axios.get(`https://www.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&regionCode=US&key=${YOUTUBE_API_KEY}`)
+      .then(res => {
+        this.setState({
+          videos: res.items,
+        })
+      })
   }
+
+
 
   render () {
 
     return (
       <div>
-        Hi
+        <nav className="navbar">
+          Navbar
+        </nav>
+        <div className="row">
+          <div classNAme="col-md-7">
+
+          </div>
+          <div className="col-md-5">
+            <div><h5>List of Videos</h5></div>
+            <VideoResults
+              videos={this.state.videos}
+            />
+          </div>
+        </div>
       </div>
     );
   }
